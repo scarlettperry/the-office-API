7cb75238053cba5f33eae514a976b8c4631c39bd
scarlettperry <si.perry15@gmail.com>
2019-05-13 21:44:00 -0400

    added error handling in Departments controller

--- app/controllers/api/v1/departments_controller.rb
+++ app/controllers/api/v1/departments_controller.rb
@@ -4,23 +4,23 @@ class Api::V1::DepartmentsController < ApplicationController
         render json: @departments
     end
 
-    #make a new department
     def create
         @department = Department.create(department_params)
-        if @department
+        if @department.valid?
             render json: @department, status: :accepted
         else
             render json: {errors: @department.errors.full_messages}, status: :unprocessable_entity
         end
     end
 
-    #edit department name
+    
     def update
         @department = Department.find(params[:id])
-        if @department
+        @department.update(department_params)
+        if @department.valid?
             render json: @department, status: :accepted
         else
-            render json: {errors: @department.errors.full_messages}, status: :unprocessable_entity
+           render json: {errors: @department.errors.full_messages}, status: :unprocessable_entity
         end
     end
 
@@ -28,4 +28,5 @@ class Api::V1::DepartmentsController < ApplicationController
     def department_params
         params.require(:department).permit(:name)
     end
+
 end
